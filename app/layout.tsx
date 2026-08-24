3e4574a5ef118902393bfd99f74777f6d5411416
williamsbezerra2706-lgtm <williamsbezerra2706@gmail.com>
2026-08-24 10:35:17 -0300

    Install Meta Pixel globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Nunito_Sans } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import './typography.css';
 import './images.css';
@@ -23,7 +24,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={nunitoSans.variable}>{children}</body>
+      <body className={nunitoSans.variable}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','1374741981502698');
+fbq('track','PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1374741981502698&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
