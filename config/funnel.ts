cf7573435f47aea1a86bf6a249b129a01b30ff2d
williamsbezerra2706-lgtm <williamsbezerra2706@gmail.com>
2026-09-08 22:38:34 -0300

    Update last chance Hotmart checkout URL

--- config/funnel.ts
+++ config/funnel.ts
@@ -3,7 +3,7 @@ export const funnelConfig = {
   currency: 'US$', fullPrice: 147, finalPrice: 17,
   checkoutUrl: 'https://pay.hotmart.com/F107343958P?off=80vtw7st&checkoutMode=10' as string, vslEnabled: false, offerTimerEnabled: false,
   exitOfferPrice: 15.90,
-  exitOfferCheckoutUrl: 'https://pay.hotmart.com/F107343958P?off=ii9p83wb' as string,
+  exitOfferCheckoutUrl: 'https://pay.hotmart.com/F107343958P?off=ii9p83wb&checkoutMode=10' as string,
   rewardDurationMs: 1050,
   increments: { q1: 20, q2: 20, q3: 20, lead: 20, q4: 20, q5: 15, q6: 15 },
   images: {
